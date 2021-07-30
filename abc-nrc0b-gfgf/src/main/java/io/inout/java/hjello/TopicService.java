79bfe8cbd90f99ab585151fbb0013bf3ca331ad0
ankit ghosh <ankitghosh1399@gmail.com>
2021-07-30 12:39:08 +0530

    first commit

---
+++ abc-nrc0b-gfgf/src/main/java/io/inout/java/hjello/TopicService.java
@@ -0,0 +1,36 @@
+package io.inout.java.hjello;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	List<ANKIT> topics=new ArrayList<>(Arrays.asList( 
+			new ANKIT("name","abc"),
+			new ANKIT("nae","wre"),
+			new ANKIT("na","qq")
+			));
+	public List<ANKIT> getTopic() {
+	    return topics;
+	}
+	
+	public ANKIT getsingletopic(String id){
+		return topics.stream().filter(i ->i.getAge().equals(id)).findFirst().get();
+	}
+	public void add(ANKIT ankit){
+		topics.add(ankit);
+	}
+
+	public void update(ANKIT ankit, String id) {
+		for (int i=0; i<topics.size();i++) {
+			if(topics.get(i).getAge().equals(id)) {
+				topics.set(i, ankit );
+			}
+		}
+	}
+	
+}
---
+++ abc-nrc0b-gfgf/src/main/java/io/inout/java/hjello/hello.java
@@ -0,0 +1,37 @@
+package io.inout.java.hjello;
+
+import java.util.List;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+	@Autowired
+	public TopicService topicservice;
+	@RequestMapping("/hello")
+	public List<ANKIT> getlist(){
+		return topicservice.getTopic();
+		
+	}
+	@RequestMapping("/hello/{abc}")	
+    public ANKIT getsingletopic(@PathVariable("abc") String id) {
+    	return topicservice.getsingletopic(id);
+    }		
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void insertTopic(@RequestBody ANKIT ankit) {
+		topicservice.add(ankit);
+	}
+
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void updateTopic(@RequestBody ANKIT ankit, @PathVariable String id ) {
+		topicservice.update(ankit, id);
+	}
+
+}
